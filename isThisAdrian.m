3011945f4cf0622828bd12748058f146cb2d006c
AdrianSantoPasqualini <45246430+AdrianSantoPasqualini@users.noreply.github.com>
2018-12-04 22:26:51 -0500

    Use Prediction Threshold given in setParameters
    It should make predictions based on the threshold chosen in setParameters. It shouldn't be fixed at 0.5.

--- isThisAdrian.m
+++ isThisAdrian.m
@@ -8,12 +8,12 @@ x = [1; imageToFeatureVector(image, "/Test-Set", false)];
 
 hx = 1./(1 + exp(-theta'*x));
 
-if hx >= 0.5
+if hx >= predictionThreshold
     disp("This is Adrian");
     disp(["There is a ", mat2str(round(hx*100)), "% chance that this is Adrian"]);
 end
                  
-if hx < 0.5
+if hx < predictionThreshold
     disp("This is Not Adrian");
     disp(["There is only a ", mat2str(round(hx*100)), "% chance that this is Adrian"]);
 end
